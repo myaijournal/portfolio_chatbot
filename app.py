5bf5bdc6e5481a2440cac5467a737c2e9e7a3d4b
Surya <suryasaith@gmail.com>
2024-08-28 16:40:43 -0500

    Updated the logic with Chat Prompts and Kwargs

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import cassio
 from langchain_openai import ChatOpenAI
-from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+#from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Cassandra
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.schema import BaseOutputParser
+from langchain.schema import StrOutputParser
 from langchain_community.chat_models import ChatOpenAI
 
 import streamlit as st
@@ -13,88 +13,112 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 
-from PyPDF2 import PdfReader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.runnables import RunnablePassthrough
 
+
+######
+
+#Function to load pdfs
 def extract_text_from_pdfs(directory):
     for filename in os.listdir(directory):
         if filename.endswith(".pdf"):
             pdf_file_path = os.path.join(directory, filename)
-            with open(pdf_file_path, 'rb') as pdf_file_obj:
-                pdf_reader = PdfReader(pdf_file_obj)
-                num_pages = len(pdf_reader.pages)
-                text = ''
-                for page in range(num_pages):
-                    page_obj = pdf_reader.pages[page]
-                    text += page_obj.extract_text()
-            yield filename, text
+            pdf_reader = PyPDFLoader(pdf_file_path)
+            docs = pdf_reader.load()
+            docs.extend(pdf_reader.load())
+######
 
 # Specify the directory containing the PDFs
 directory = 'portfolio_pdfs'
+docs = []
 
-# Initialize an empty string to accumulate all text
-all_text = ''
+extract_text_from_pdfs(directory)
 
-# Use the generator to accumulate text from all PDFs
-for filename, text in extract_text_from_pdfs(directory):
-    all_text += text
+######
 
-load_dotenv()
+load_dotenv() #load env variables
 
 ASTRA_DB_APPLICATION_TOKEN = os.getenv('ASTRA_DB_APPLICATION_TOKEN')
 ASTRA_DB_ID = os.getenv('ASTRA_DB_ID')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo", temperature=0.5)
+######
+
+#define llm and embeddings
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo", temperature=0.5, max_tokens=400)
 embedding = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+
+######
 
 cassio.init(token=ASTRA_DB_APPLICATION_TOKEN, database_id=ASTRA_DB_ID) # Initialize CassandraDB
 
-text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=1000,
-    chunk_overlap=200,
-    length_function=len,
+######
+
+#Splitting docs into chucks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = text_splitter.split_documents(docs)
+
+######
+
+#initialising vector store object
+astra_vectorstore = Cassandra.from_documents(
+    documents=documents,
+    embedding=embedding, 
+    table_name="portfolio_vectordb")
+
+
+######
+
+# Index wrapping for faster query
+#class CustomVectorStoreIndexWrapper(VectorStoreIndexWrapper):
+    # def __init__(self, vectorstore):
+    #     super().__init__(vectorstore)
+
+#astra_vector_index = VectorStoreIndexWrapper(vectorstore=astra_vectorstore)
+
+######
+
+# Define the prompt template
+
+prompt = ChatPromptTemplate.from_template("""
+    
+    You are an AI assistant who answers user queries about Surya's Portfolio. 
+                                          
+    Generate a concise response only using the following pieces of retrieved text to answer the question. 
+                                          
+    If you don't find a perfect match, politely punt.
+                                                                   
+    Question: {query}
+                                          
+    Context: {context}                                  
+                                          
+    Answer:
+                                          
+                                          """)
+
+######
+
+# Retrieve and generate
+
+retriever = astra_vectorstore.as_retriever(search_kwargs={"k": 2})
+
+######
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Create document chain
+chain = (
+    {"context": retriever | format_docs, "query": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
 )
 
-astra_vectorstore = Cassandra(
-    embedding=embedding, table_name="portfolio_vectordb", session=None, keyspace=None)
+######
 
-astra_vectorstore.add_texts([all_text[:2000]])
-
-class CustomVectorStoreIndexWrapper(VectorStoreIndexWrapper):
-    def __init__(self, vectorstore):
-        super().__init__(vectorstore)
-
-astra_vector_index = VectorStoreIndexWrapper(vectorstore=astra_vectorstore)
-
-# # Define the prompt template
-# prompt_template = """
-# Given the context: {query}, generate a concise response. You are an AI assistant who answers user queries based on the provided search context. If you don't find a perfect match, politely punt.
-# """
-# user_template = "{query}"
-
-# chatprompt = ChatPromptTemplate.from_messages([
-#     ("system", prompt_template),
-#     ("human", user_template)
-# ])
-
-# # Define the output parser
-# class CleanOutput(BaseOutputParser):
-#     def parse(self, output):
-#         return output.strip()
-
-# # Initialize the chain
-# chain = chatprompt|llm|CleanOutput()
-
-# def handle_query(query):
-#     # Search the Vector DB for the most similar embeddings
-#     answer = astra_vector_index.query(query, llm=llm).strip()
-
-#     # Run the sequential chain
-#     response = chain.invoke(relevant_text)
-
-#     return response
-
+# Streamlit interface
 
 st.title("🌟 Surya's Portfolio ChatBot 🤖")
 st.header("Welcome to Surya's Portfolio!")
@@ -111,5 +135,5 @@ user_query = st.text_input("Ask me a question...", placeholder="Type your questi
 
 # Button to trigger the chatbot response with a custom label
 if st.button("Get Answer 🚀"):
-    answer = astra_vector_index.query(user_query, llm=llm).strip()
+    answer = chain.invoke(user_query)
     st.write(answer)
